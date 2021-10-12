4de423768cbbcb26d8b537b16a970f866a1e7900
plumy-chan <92406075+plumy-chan@users.noreply.github.com>
2021-10-12 16:55:36 -0700

    Update list.js

--- list.js
+++ list.js
@@ -1,7 +1,7 @@
 // One Google Drive file ID per line
 let clipIDs = `
-14QvMxODh8X-9a5cljt3KA8bg4vBA-2UO
-1ya0U5NgbnhdHhSannCEv72o6slmoAlYk
-1-jfdX4_NXwDIQaRslOg3MFXi9hENnTpu
-10RbaQJUUYgqJOa3Leqcy4uflitmGxkjG
+14QvMxODh8X-9a5cljt3KA8bg4vBA-2UO //20210607.something something something something.1209627103.mp4
+1rbrT55kk8HfjcgtocTWLMqB-BKWD02B1 //20210126.peepoRun .1023312631.mp4 
+1-jfdX4_NXwDIQaRslOg3MFXi9hENnTpu //signal.mp4
+10RbaQJUUYgqJOa3Leqcy4uflitmGxkjG //shashasha.mp4
 `;
