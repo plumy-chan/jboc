fd9a78e004664f0770cb403cfd99b60dc7bdb8d3
plumy-chan <92406075+plumy-chan@users.noreply.github.com>
2021-10-14 23:28:06 +0200

    Update list.js

--- list.js
+++ list.js
@@ -4,6 +4,60 @@ let clipIDs = ["14QvMxODh8X-9a5cljt3KA8bg4vBA-2UO", //20210607.something somethi
                "1-jfdX4_NXwDIQaRslOg3MFXi9hENnTpu", //signal.mp4
                "10RbaQJUUYgqJOa3Leqcy4uflitmGxkjG", //shashasha.mp4
                "1gQz98mabHT6Xvmau6ypN_xVVNLHMseQN", //20210401._Something A Lot_ - Yummy.1116194705.mp4
+               "1kJYC1uWM9D_0N2XDREN8OiezBD2ICwpP", //02 pc cosplay muted
+               "1YJHgtYgWufKCQ8G22Tau2AgLlcuAd1ep", //too far myriel
+               "1z9yQlp0lU12N10jcyKKGVudUsQ5KpYGS", //motorbike
+               "1-qxIN2j0mGCEFesF-gNpa8B4DDQAsk-u", //candy jumpscare
+               "1Mr6ivWMyq2Q0huZ7t_lDThRenTOHzvNc", //grappling
+               "1mieQOPBdK4tpYZ58WsUzN0QyLsDJePvV", //new house
+               "1a_bbdRyfMADdgv5RjHrEUGAigPmLYl_i", //sora wet
+               "1yQG_pEdTDKYtEL_ro0H5LHztDotUVbWF", //too cool
+               "1MNPKYOL0p-FzZWsfC5YDBgGd21Ftj3qn", //shy flushed emote
+               "1SD4WAWsfyVdqg_c_0oo0iT2fgOEwp5ht", //special amf
+               "15vC3SPnc3R_d7akBqQcVWTGD9bsfhcXg", //dumb
+               "1xSmvvI-5XJYc0Vz4q8PIeJ768286vT4w", //taeyeon eye
+               "1r1I5KKq5FUa3HcjQs1-8MinFMtNsvMPG", //teacher ponytail
+               "1SkumLqK96IqRlOUb7CtDqAHWojtmJGFy", //bunny
+               "1euhLc45JpUDrTUuBdvfZSA6j2Zazp540", //blood maid
+               "1Mcb8qDK33iFe7pnG4cTnZ3fBenio7Ezt", //fertilizer
+               "14fYUHUCReEL0l0HVeYvAYGjZFLKd-4Lu", //kiss camera
+               "1-snTDpen4W2yH9ne18PB83KS_NgfBGcZ", //backstreet boys
+               "14xp34F-Jze4AyreT8jR5VeI1KjYVfLl0", //cafe 5 hit
+               "1Ph5Df1pHcASKAv7_6Wky7YD1M2SJdGwJ", //pool hit
+               "1abdnwchhBBS9ofTSiFZn-4CXv-3gD5Rx", //3 person 02
+               "19CDqLvRd-73E28sE3n9JlXeFTuv6_Woe", //airplane
+               "14xawq3dWTWSR9CFUMSL1J_LfLtXKjXbW", //alcohol free
+               "1_aJjxrfB1kc5yrnr769mH3GSbJVYhW_N", //allen shirt
+               "1S3GQ_Z-8O7eknloEn6LM4Wo0-v-zIhkF", //ASAP
+               "1j1L9-XzvtyZgty4CEjMMjbmyC35YOanz", //atago sword
+               "1GT_1kJqQiR3fldscg-cxTCHKGOul12lh", //ayaya rap
+               "1ozLs-NfIEHSFZCg2rWrbJhOk_ItueAQG", //belly
+               "1Naa_mGkoFpp1I3Kuq04YmDWkAdrUgE_H", //close eyes kill
+               "1HVFx2Fo5kSM07EbFIFsFChN84npgq-Or", //cup of tea
+               "1J17-PoC5b60_dmZQPAiEf9UiMmVID5qk", //dundun amber
+               "18s77nlAGHfifMsbJWWPv_RlgSGK55bOZ", //find me 
+               "1F_5sR3U7ylmh8eIQDDCHIZa6sNc8wpO-", //girlfriend vr
+               "14e03qIXecrxiapDNNtEwFj2ZNK0TN7P9", //happy
+               "1C8a_eEZ8BDXPvuNlW16pMYETnTLa5dTl", //horror lowered
+               "1ejSCkA8NGMcUVTr_TWuUjDzgSEMCCVxg", //horror maze
+               "1eE2wQUUJME2YQSIZxx3-bu0OLpykUYEn", //jasmine
+               "1Ocany4nNtXgka7gfXvRnhBckzq3vFP0z", //jebait 02
+               "1IBJlrXEyNWMb_UB6XD0F4oAVcj7uVgqy", //microphon
+               "1eMlM5We_528Sx1-UNITsPDQ7Q2Z7TQky", //pc glasses punch
+               "1Btavg4xO2f3f6YhAQfcY66BFDWTZCaFE", //my eye
+               "195Qsl64wGymEhWnJNA0ScqbGLXvpjsKP", //bed peekaboo
+               "1IKOfJD9rWmDwkeyuLf8AsXgxDdaIbxW0", //club dance
+               "1Kw8p__s8Im0M9TVgHNliVbS_DWJ1Jlzb", //knife ayaya
+               "1eD64nR4PikJmU6ivNR2ScdGxZblH4bVD", //cake cutting
+               "1tBxh-z77FifwY2PTRGlb_Hz7Af2h1-IU", //whip
+               "1aWUfyJWdyGUWe325NcxyUsfCx1RC9-tO", //gun
+               "", //
+               "", //
+               "", //
+               "", //
+               "", //
+               "", //
+               "", //
                "", //
                "", //
                "", //
