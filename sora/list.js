80d1b01004b55e07a5313be04ed3717fda9179ce
Sorabbang629 <100978471+Sorabbang629@users.noreply.github.com>
2022-03-05 12:55:19 +0900

    Update list.js

--- sora/list.js
+++ sora/list.js
@@ -15,9 +15,23 @@ let clipIDs = ["1QrPh689ORz4lB9O1LmiwPgl0fj3oOVZk", //고양이_그릇
                "1R9NachLk0fz2O1T29_LbBk45qimYWQFm", //하츄비언니가레이드해줬을때
                "12m4cdDNep-Tc_XGjGwgyWr3bfUoNoQKt", //하츄비언니에게레이드해줬을때
                "1NFq1P5i-Q1vxSf-4kt3Ci0ubbP9pT0RM", //SOS
-               "", //
-               "", //
-               "", //
+               "1GsGLZ2QlQuYXtRxF24FB3hVLCfXBfheU", //소라부리또
+               "1EWGYsQzB1ufRLp9e712dgANnMukEFaQj", //댄스_츄츄송_하츄비언니서연
+               "1ES87AfaY-XFsxx0k6QhdGw-ajqNunpf0", //댄스_빨개요_노란투피스
+               "13ZtogRRG_Ul0XuWeOGK7QCaXApj-r_KI", //운동_베리
+               "15BajvziST7blntcIPVjC5xWu3vHaqCHp", //댄스_몽키매직_식스팩티
+               "1XZxkKw3_LPPogb9LVxlN6twwRuWr6B74", //애교_쥐잡이송
+               "1VG3ZN32dpkuekxZUYg2Gf_oF-kjjbuu7", //애교_도네를주세요
+               "1q32O4m6WiHDwGlCYWeJFe8Q411F2G8eU", //노래_이수
+               "1kXM-awSatbZvGdSwJs32TpQ95sv0Bh9c", //댄스_깡_밍미단이
+               "1u_y5DfG247DIvniUd3Lffp5SMjeHj7WD", //고양이_시루설탕_거실의자
+               "1ICGupy2wmhxg1oeJrf5zIxhRMeiIKh1v", //milk first
+               "18pbOPUfAsttP-68wHck_tolpcMmd0YKi", //코스프레_한푸_설기
+               "15eNM3ICM3hkKAGqO05tgT4R-tSYGeu8a", //코스프레_에밀리아
+               "1C8IE0kV2QQWFfMjS952fL_MPCm1Pms0W", //여행_제주도_우도_역주행
+               "1dDsjYjlwI3_CZIh6M8TpWr2N0NzpRb6R", //코스프레_간호사_설탕
+               "1UAwENAMlRBai3n4IcOOBU8tHCyB3isxK", //여행_제주도_비키니_밍유니
+               "1iY9_j20tCtgjYqMMvriWdl-LB7BsOtfH", //댄스_코카인_유캠_
                "", //
                "", //
                "", //
