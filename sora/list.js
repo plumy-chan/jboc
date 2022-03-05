425b6f7b1a7b15eaf9c9d34791d59d0de344250a
Sorabbang629 <100978471+Sorabbang629@users.noreply.github.com>
2022-03-05 09:49:15 +0900

    Update list.js

--- sora/list.js
+++ sora/list.js
@@ -1,8 +1,8 @@
 // One Google Drive file ID per line
-let clipIDs = ["", //
-               "", //
-               "", //
-               "", //
+let clipIDs = ["1QrPh689ORz4lB9O1LmiwPgl0fj3oOVZk", //고양이_그릇
+               "1FA4rxtgqYxw099qUILIDRmF93QQ4oOxt", //고양이_설기아가들_처음눈뜸
+               "1lDvF0rvQbf973venD84D8op_-F318hmR", //댄스_같은시간
+               "1sI6s2euuJR7K7j6E3Bt95ims-E1yU-9j", //아이폰13pro
                "", //
                "", //
                "", //
