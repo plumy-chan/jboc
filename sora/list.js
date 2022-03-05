8484a49ec01b8cf899e66c34285a088b9fae34d2
Sorabbang629 <100978471+Sorabbang629@users.noreply.github.com>
2022-03-05 12:03:12 +0900

    Update list.js

--- sora/list.js
+++ sora/list.js
@@ -3,18 +3,18 @@ let clipIDs = ["1QrPh689ORz4lB9O1LmiwPgl0fj3oOVZk", //고양이_그릇
                "1FA4rxtgqYxw099qUILIDRmF93QQ4oOxt", //고양이_설기아가들_처음눈뜸
                "1lDvF0rvQbf973venD84D8op_-F318hmR", //댄스_같은시간
                "1sI6s2euuJR7K7j6E3Bt95ims-E1yU-9j", //아이폰13pro
-               "", //
-               "", //
-               "", //
-               "", //
-               "", //
-               "", //
-               "", //
-               "", //
-               "", //
-               "", //
-               "", //
-               "", //
+               "1qoDOSeX7XwrHKLISr4TEAUIu7SGqQSsm", //고양이_아가셋
+               "1Ny5Ms48txZYmjDlsDbUwLDacAXweRD4e", //댄스_굿보이_루돌프잠옷
+               "1M97oNv0SqbMC97CasdKGLGZGwt_tX4lY", //댄스_블루문
+               "1JLsS_WsCeI--vqu9agO9CgpVHsXvrOCf", //댄스_조커댄스_밍
+               "1n41rSzMX-h2ER61niyD-BGioO7HekzvJ", //댄스_퀸덤_제주도야외
+               "11bOzEqn3No95wiN55yZeeVl90WgVWtCA", //댄스_툥이
+               "1IU4SYw60HgnFbRSYXE1jxXmqiG-txcbB", //댄스_stay_베리
+               "1kAAt8YcVgsb0yz9s6QelNCLteZuU8a3D", //불태피짱_버블리밍유니
+               "1XnZ2vOtUFa7ybB28AsYrziVeAYh0HGBG", //코스프레_스파이더맨_호캉스
+               "1R9NachLk0fz2O1T29_LbBk45qimYWQFm", //하츄비언니가레이드해줬을때
+               "12m4cdDNep-Tc_XGjGwgyWr3bfUoNoQKt", //하츄비언니에게레이드해줬을때
+               "1NFq1P5i-Q1vxSf-4kt3Ci0ubbP9pT0RM", //SOS
                "", //
                "", //
                "", //
