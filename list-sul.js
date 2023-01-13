aa0023a330289c48e6089caba5de690b1f8d7166
plumy-chan <92406075+plumy-chan@users.noreply.github.com>
2023-01-13 14:07:11 -0800

    Add files via upload

---
+++ list-sul.js
@@ -0,0 +1,6 @@
+// One s-ul file ID per line
+let clipIDs = ["IpLktnoN", //motorbike
+               "LiyS9jEi", //yummy something alot 
+               "y3VkqRos", //signal.mp4
+               "TdhaPbv4", //shashasha.mp4
+''];
